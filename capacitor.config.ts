56910bf57445154574f295bfc403b4409228f1fa
DarkIsDude <edouard.comtet@gmail.com>
2024-08-01 13:55:04 +0200

    💥 add NewRelic dependencies and make sure everything is broken

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,18 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.myapp',
   appName: 'my-app',
-  webDir: 'build'
+  webDir: 'build',
+
+  plugins: {
+    CapacitorHttp: {
+      enabled: true,
+    },
+  },
+
+  server: {
+    url: 'http://192.168.0.23:3000',
+    cleartext: true,
+  },
 };
 
 export default config;
--- src/index.js
+++ src/index.js
@@ -4,8 +4,47 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './i18n';
+import { Capacitor } from '@capacitor/core';
+
+async function initNewrelic() {
+  if (!Capacitor) {
+    return;
+  }
+
+  if (Capacitor.getPlatform() === 'web') {
+    return;
+  }
+
+  console.info('Initializing New Relic for Capacitor');
+
+  const { NewRelicCapacitorPlugin } = await import('@newrelic/newrelic-capacitor-plugin');
+
+  const appToken = "eu01xxbd3be6889f366a070da4d61a7c5f76868a60-NRMA";
+
+  let agentConfig = {
+    analyticsEventEnabled: true,
+    crashReportingEnabled: true,
+    interactionTracingEnabled: true,
+    networkRequestEnabled: true,
+    networkErrorRequestEnabled: true,
+    httpResponseBodyCaptureEnabled: true,
+    loggingEnabled: true,
+    logLevel: "info",
+    webViewInstrumentation: true,
+    sendConsoleEvents: true,
+    fedRampEnabled: false,
+    offlineStorageEnabled: true,
+    backgroundReportingEnabled: false,
+    newEventSystemEnabled: true
+  }
+
+  NewRelicCapacitorPlugin.start({appKey:appToken, agentConfiguration:agentConfig})
+}
+
+initNewrelic();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
